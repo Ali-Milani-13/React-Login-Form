aa22dc1fd97337fec0ce92620838d6348af79084
Ali Milani <haythamtemplar@hotmail.com>
2022-04-03 20:55:28 +0430

    Form component created:   src/components/Form/Form.js 	Input component created:   src/components/Form/Input.js 	Router created for multipage functionality:   src/routes/Router.js

---
+++ src/routes/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "../views/pages/signup/SignUp";
+import Login from "../views/pages/Login/Login";
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/Login" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default Router;
