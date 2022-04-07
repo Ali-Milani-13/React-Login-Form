bff21a068bd0fcb03521be85da4f81608a119c3f
Ali Milani <haythamtemplar@hotmail.com>
2022-04-07 18:48:54 +0430

    Scss for Input component:   src/scss/Input/Input.module.scss 	Scss for LoginForm component:   src/scss/LoginForm/LoginForm.module.scss 	Scss for Navbar component:   src/scss/Navbar/Navbar.module.scss 	Scss for Snackbar component:   src/scss/Snackbar/Snackbar.module.scss 	Login component completed:   src/views/Login/Login.js 	Sign up component completed:   src/views/signup/SignUp.js

--- src/views/signup/SignUp.js
+++ src/views/signup/SignUp.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Form from "../../../components/Form/Form";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return <Form />;
 };
 
 export default SignUp;
